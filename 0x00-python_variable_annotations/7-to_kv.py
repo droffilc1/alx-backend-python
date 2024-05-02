4c1a0301c51857cc85d7cc595bdff8af2a4e3369
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-05-02 12:13:05 +0300

    fix: requirement fail

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,7 +4,8 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """Type-annotated function that takes a string and an int OR float as arguments
+    """Type-annotated function that takes a string and an int
+        OR float as arguments
     Returns a tuple.
     """
     return (k, v ** 2)
