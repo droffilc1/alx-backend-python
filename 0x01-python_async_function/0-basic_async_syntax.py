9d7be022fee1a4beca70b1215a427cea64f23f86
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-05-06 12:38:18 +0300

    feat: the basics of async

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" 0-basic_async_syntax.py """
+
+import random
+
+
+async def wait_random(max_delay=10):
+    """An asynchronous coroutine that takes in an integer argument (max_delay,
+    with a default value of 10) named wait_random that waits for a random delay
+    between 0 and max_delay (included and float value) seconds and eventually returns it.
+    """
+    # Return a random floating point number
+    return random.uniform(0, max_delay)
