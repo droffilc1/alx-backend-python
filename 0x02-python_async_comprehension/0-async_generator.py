3f8652472a44d1b8b532d9892bd72d19a43c44bf
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-05-07 11:50:54 +0300

    feat: async comprehensions

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,7 +6,7 @@ import asyncio
 import random
 
 
-async def async_generator() -> float:
+async def async_generator():
     """A coroutine called async_generator that takes no arguments.
     The coroutine will loop 10 times, each time asynchronously wait 1 second,
     then yield a random number between 0 and 10
---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" 1-async_comprehension.py """
+
+from typing import List
+import asyncio
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Collects 10 random numbers using an async comprehensing
+    over async_generator,
+    Returns the 10 random numbers
+    """
+    return [i async for i in async_generator()][:10]
