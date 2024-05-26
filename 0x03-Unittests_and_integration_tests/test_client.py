37e6b102bd89f115574c9bd8d4eda6b8911a5e5e
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-05-26 12:38:30 +0300

    test: more patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 Implements unit test for client.
 """
 import unittest
-from typing import Any, Dict
+from typing import Any, Dict, List
 from unittest.mock import PropertyMock, patch
 from parameterized import parameterized
 from client import GithubOrgClient
@@ -25,6 +25,9 @@ class TestGithubOrgClient(unittest.TestCase):
     @patch('client.GithubOrgClient.org', new_callable=PropertyMock)
     def test_public_repos_url(self, mock_org: PropertyMock) -> None:
         """Tests `GithubOrgClient._public_repos_url` method."""
+        # source:
+        # https://stackoverflow.com/questions/11836436/how-to-mock-a-\
+        # readonly-property-with-mock
         # Define a known payload for mocking
         mock_payload: Dict[str, str] = {
             "repos_url": "https://api.github.com/orgs/test_org/repos"
@@ -38,6 +41,42 @@ class TestGithubOrgClient(unittest.TestCase):
         # Assert that _public_repos_url return the expected repos_url
         expected_url: str = mock_payload['repos_url']
         self.assertEqual(client._public_repos_url, expected_url)
+
+    @patch('client.get_json')
+    @patch('client.GithubOrgClient._public_repos_url',
+           new_callable=PropertyMock)
+    def test_public_repos(self, mock_public_repos_url: PropertyMock,
+                          mock_get_json: Any) -> None:
+        """Test `GithubOrgClient.public_repos` method."""
+        # Define a known payload for the mocked _public_repos_url
+        mock_public_repos_url.return_value = \
+            "https://api.github.com/orgs/test_org/repos"
+
+        # Define a known payload for the mocked get_json
+        mock_repos_payload: List[Dict[str, Any]] = [
+            {"name": "repo1", "license": {"key": "mit"}},
+            {"name": "repo2", "license": {"key": "apache-2.0"}},
+            {"name": "repo3", "license": {"key": "mit"}},
+        ]
+        mock_get_json.return_value = mock_repos_payload
+
+        # Instantiate the client
+        client = GithubOrgClient("test_org")
+
+        # Call the public_repos method and assert the expected result
+        expected_repos_mit: List[str] = ["repo1", "repo2", "repo3"]
+        self.assertEqual(client.public_repos(), expected_repos_mit)
+
+        # Test with a specific license filter
+        expected_repos_mit: List[str] = ["repo1", "repo3"]
+        self.assertEqual(client.public_repos(
+            license="mit"), expected_repos_mit)
+
+        # Assert that the mocked property and the mocked get_json were
+        #  called once
+        mock_public_repos_url.assert_called_once()
+        mock_get_json.assert_called_once_with(
+            "https://api.github.com/orgs/test_org/repos")
 
 
 if __name__ == '__main__':
